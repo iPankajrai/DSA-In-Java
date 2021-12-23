bbf0e6ef3b84d647efbaabc882ae0c106b59ca8e
Pankaj Rai <pankaj200497@gmail.com>
2021-12-24 02:30:35 +0530

    added folder for 2D Arrays

---
+++ src/TwoDArrays/Basics.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package TwoDArrays;
+
+/**
+ * @author Pankaj Rai
+ *
+ */
+public class Basics {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+
+	}
+
+}
